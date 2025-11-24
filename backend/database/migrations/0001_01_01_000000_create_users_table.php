d45802056f63cbebcc25b82d5fd1a21a3d113e76
rh64s <cchelovekov64@gmail.com>
2025-11-24 13:21:06 +0700

    refactor: replace project

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('full_name', 255);
+            $table->string('email', 255)->unique()->nullable();
+            $table->string('phone', 20)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->text('password_hash');
+
+            $table->boolean('is_admin')->default(false);
+//            $table->boolean('gosservices_bound')->default(false);
+//            $table->string('gosservices_id', 255)->unique()->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
