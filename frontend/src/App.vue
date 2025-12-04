3fbe94d6a4f87babd2858e888883cff4f80419db
Daniil Kiryakov <kiriakovdaniil717@gmail.com>
2025-12-05 00:35:58 +0700

    add support message

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 
 import AppHeader from "@/components/shared/AppHeader.vue";
+import SupportMessage from "@/components/shared/SupportMessage.vue";
 </script>
 
 <template>
-<app-header />
+  <app-header />
+
+  <support-message />
 </template>
 
 <style lang="scss">
-body {
+#app {
   margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 100px;
 }
 </style>
---
+++ frontend/src/components/shared/SupportMessage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<section class="support-message">
+  <div class="support-message__inner">
+    <p class="support-message__content">В этот трудный момент мы готовы поддержать вас — с уважением, заботой и вниманием к <span class="support-message__content--underline">каждой</span> детали.</p>
+  </div>
+</section>
+</template>
+
+<style scoped lang="scss">
+.support-message {
+  width: 100%;
+
+  &__inner {
+    margin: 0 auto;
+    max-width: 1540px;
+    width: 100%;
+    @extend .base-block;
+    padding: 40px;
+  }
+
+  &__content {
+    font-size: 1.5rem;
+    color: $color-black;
+    font-weight: $font-weight-light;
+    font-style: $font-style-light;
+    max-width: 1065px;
+    text-align: center;
+
+    &--underline {
+      text-decoration-line: underline;
+      text-decoration-style: wavy;
+      text-decoration-color: $color-gray;
+    }
+  }
+}
+</style>
