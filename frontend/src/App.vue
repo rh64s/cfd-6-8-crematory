7c4c2993e396c817edd1492021e1f58f1379e500
Daniil Kiryakov <kiriakovdaniil717@gmail.com>
2025-12-04 20:36:50 +0700

    add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 
+import AppHeader from "@/components/shared/AppHeader.vue";
 </script>
+
 <template>
-
+<app-header />
 </template>
+
 <style lang="scss">
-
-
+body {
+  margin-top: 50px;
+}
 </style>
---
+++ frontend/src/components/shared/AppHeader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import NavigationLink from "@/components/UI/NavigationLink.vue";
+import {navLinkList} from "@/utilities/navLink.ts";
+import Button from "@/components/UI/Button.vue";
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__inner">
+      <h1>Крематорий</h1>
+
+      <nav class="header__navigation">
+        <p v-for="link of navLinkList" :key="link.text">
+          <navigation-link :link="link.text" :href="link.link">{{ link.text }}</navigation-link>
+        </p>
+      </nav>
+
+      <Button>войти</Button>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  font-size: 1.5rem;
+  color: $color-gray;
+  font-weight: $font-weight-bold;
+}
+
+.header {
+  &__inner {
+    max-width: 1540px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 0 10px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__navigation {
+    display: flex;
+    align-items: center;
+    column-gap: 40px;
+  }
+}
+</style>
