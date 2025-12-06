b21c1a96c442554fe41777b3e72d226a80e0cfaf
Daniil Kiryakov <kiriakovdaniil717@gmail.com>
2025-12-07 00:57:31 +0700

    add section not alone

--- frontend/src/pages/AppHome.vue
+++ frontend/src/pages/AppHome.vue
@@ -3,12 +3,14 @@
 import SupportMessage from "@/components/landing/SupportMessage.vue";
 import AboutUs from "@/components/landing/AboutUs.vue";
 import CardServices from "@/components/landing/CardServices.vue";
+import NotAlone from "@/components/landing/NotAlone.vue";
 </script>
 
 <template>
   <support-message />
   <about-us />
   <card-services />
+  <not-alone />
 </template>
 
 <style scoped lang="scss">
