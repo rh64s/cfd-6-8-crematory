9dd5a3fcdbcdfd25b8f2844eb5e7befa8968d06c
rh64s <cchelovekov64@gmail.com>
2025-12-14 20:19:25 +0700

    replace: sections for requests

--- backend/app/Http/Requests/User/UpdateProfileRequest.php
+++ backend/app/Http/Requests/User/UpdateProfileRequest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\User;
 
+use App\Http\Requests\ApiFormRequest;
 use Illuminate\Validation\Rule;
 
 class UpdateProfileRequest extends ApiFormRequest
