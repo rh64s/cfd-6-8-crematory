48710af8dfa695d830981499832d5785e84c5fc0
elfi <gugl6369@gmail.com>
2025-12-15 23:29:02 +0000

    add(index.ts): axios and add package

---
+++ frontend/src/utilities/API.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import type {UserRegister} from '@/utilities/types.ts'
+
+const localhost =  'https://example.com/api'
+
+
+export const register = (userData :UserRegister) => axios.post(localhost + "/register",  userData)
+
+
+
+
+
+
+
+
+/*
+const login = (userData :UserLogin) => axios.post(localhost + "/login",  userData)
+
+
+.then( function (response){
+  console.log(response)
+})
+  .catch(function (error){
+    console.log(error)
+  })
+
+
+“first_name”: "Аркадий",
+  “last_name”: "Александрович",
+  "patronymic": "Паровозов",
+  "login": "ilovemy228",
+  "phone": "+88005553535",
+  "email": "example@example.com",
+  "password": "QWEasd123"
+*/
