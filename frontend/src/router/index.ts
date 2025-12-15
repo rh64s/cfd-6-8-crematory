cf222069c2e871fcd3f888797404a2d9ff61f1dd
elfi <gugl6369@gmail.com>
2025-12-15 23:30:47 +0000

    add(app.vue, AppRegistration.vue, index.ts): routers, app registration

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AppHome from "@/pages/AppHome.vue";
+import AppRegistration from "@/pages/AppRegistration.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: AppHome,
+    },
+    {
+      path: '/register',
+      name: 'registration',
+      component: AppRegistration,
+    },
+  ],
 })
 
 export default router
