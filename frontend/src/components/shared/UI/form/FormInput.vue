0a8dc753d43c67f56ba58d8959ba9a9b53b53727
elfi <gugl6369@gmail.com>
2025-12-16 13:01:18 +0000

    fix(vue): new design, start create form

--- frontend/src/components/shared/UI/form/FormInput.vue
+++ frontend/src/components/shared/UI/form/FormInput.vue
@@ -1,26 +1,37 @@
 <script setup lang="ts">
 </script>
+
+
 <template>
-  <label class="label" id=""></label>
-  <input class="input" type="text" placeholder="" />
+  <label class="label">
+    <input class="input" type="text" placeholder="" />
+  </label>
 </template>
+
+
 <style lang="scss" scoped>
-.input{
+.label{
   color: $color-gray;
-  font-family: "Inter", sans-serif;
-  border: none;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.25);
   border-radius: 33px;
-  border: 1px solid rgba(217, 218, 242, 1);
+  border:  1px solid rgba(217, 218, 242, 1) ;
   padding: 13px 30px;
+  transition: border .2s ease-in-out;
 }
-.input:hover{
-  color: rgba(105, 105, 105, 1);
+.input{
+  font-family: "Inter", sans-serif;
+  border: none;
+}
+.label:hover{
+  border: 3px solid rgba(217, 218, 242, 1);
 }
 .input:focus{
   color: $color-gray;
   font-family: "Inter", sans-serif;
   outline: none;
   border: 1px solid rgba(217, 218, 242, 1);
+}
+.input:focus .label{
+  border: 3px solid rgba(217, 218, 242, 1);
 }
 </style>
--- frontend/src/pages/AppRegistration.vue
+++ frontend/src/pages/AppRegistration.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+  import FormInput from "@/components/shared/UI/form/FormInput.vue"
 
 </script>
 
 <template>
-<h1>sdfsdfds</h1>
+  <section>
+    <h1>Регистрация</h1>
+    <form class="form">
+      <div>
+        <form-input type="text"></form-input>
+        <form-input type="date"></form-input>
+      </div>
+
+
+    </form>
+  </section>
 </template>
 
 <style scoped lang="scss">
