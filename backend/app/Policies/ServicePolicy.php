101ab950001541486cc1e56052875affd7931562
rh64s <cchelovekov64@gmail.com>
2025-12-17 13:27:51 +0700

    del: remove unused policy defines

--- backend/app/Policies/ServicePolicy.php
+++ backend/app/Policies/ServicePolicy.php
@@ -9,22 +9,6 @@ use Illuminate\Auth\Access\Response;
 class ServicePolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Service $service): bool
-    {
-        return true;
-    }
-
-    /**
      * Determine whether the user can create models.
      */
     public function create(User $user): bool
