db03af5d6c80445bbcb98af5861a27910a391107
rh64s <157222722+rh64s@users.noreply.github.com>
2025-12-18 20:29:41 +0700

    Revert "Dev"

--- frontend/src/utilities/API/API.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import type {UserRegister, UserLogin} from '@/utilities/API/typesApi.ts'
-
-const localhost =  'http://localhost/api'
-
-
-export const register = (userData :UserRegister) => axios.post(localhost + "/auth/register",  userData)
-export const login = (userData :UserLogin) => axios.post(localhost + "/auth/login",  userData)
