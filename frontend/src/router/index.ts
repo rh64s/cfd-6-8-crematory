050bb05c62674f90c7ad148ab96f2ac0989af594
elfi <gugl6369@gmail.com>
2025-12-18 03:33:22 +0000

    add(ts, vue): login finish

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from "@/pages/AppHome.vue";
 import AppRegistration from "@/pages/AppRegistration.vue";
+import AppLogin from "@/pages/AppLogin.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'registration',
       component: AppRegistration,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: AppLogin,
     },
   ],
 })
--- frontend/src/utilities/API/API.ts
+++ frontend/src/utilities/API/API.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import type {UserRegister} from '@/utilities/API/typesApi.ts'
+import type {UserRegister, UserLogin} from '@/utilities/API/typesApi.ts'
 
 const localhost =  'http://localhost/api'
 
 
 export const register = (userData :UserRegister) => axios.post(localhost + "/auth/register",  userData)
+export const login = (userData :UserLogin) => axios.post(localhost + "/auth/login",  userData)
