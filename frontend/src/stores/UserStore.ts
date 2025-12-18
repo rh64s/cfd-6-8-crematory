6289f67ecf2f48cc8ac25f082d2c9caa8460b0f8
elfi <gugl6369@gmail.com>
2025-12-18 02:03:17 +0000

    add(ts): clean code

--- frontend/src/stores/UserStore.ts
+++ frontend/src/stores/UserStore.ts
@@ -1,4 +1,3 @@
-import {ref, computed, createApp} from 'vue'
 import { defineStore } from 'pinia'
 import type {UserLogin, UserRegister} from '@/utilities/API/typesApi.ts'
 import { register } from '@/utilities/API/API.ts'
@@ -6,8 +5,6 @@ import router from "@/router";
 
 
 export const useUsersStore = defineStore('user', () => {
-  //const SaveUser = (data: UserRegister) => {
-  //}
   const CreateUser = (user : UserRegister) => {
     return register(user)
     .then( function (response){
--- frontend/src/utilities/API/API.ts
+++ frontend/src/utilities/API/API.ts
@@ -5,31 +5,3 @@ const localhost =  'http://localhost/api'
 
 
 export const register = (userData :UserRegister) => axios.post(localhost + "/auth/register",  userData)
-
-
-
-
-
-
-
-
-/*
-const login = (userData :UserLogin) => axios.post(localhost + "/login",  userData)
-
-
-.then( function (response){
-  console.log(response)
-})
-  .catch(function (error){
-    console.log(error)
-  })
-
-
-“first_name”: "Аркадий",
-  “last_name”: "Александрович",
-  "patronymic": "Паровозов",
-  "login": "ilovemy228",
-  "phone": "+88005553535",
-  "email": "example@example.com",
-  "password": "QWEasd123"
-*/
